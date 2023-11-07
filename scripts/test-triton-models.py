fc1cc4fb9a3ccd1c46ab803f80c6ea31256d3d79
Jeremy Metz <metz.jp@gmail.com>
2023-11-07 20:53:36 +0100

    Working cellpose-python test, stardist is WIP

--- scripts/test-triton-models.py
+++ scripts/test-triton-models.py
@@ -1,6 +1,9 @@
 import asyncio
+import traceback
+import imageio
 from imjoy_rpc.hypha import connect_to_server
 import requests
+
 
 async def main():
     server = await connect_to_server(
@@ -28,5 +31,38 @@ async def main():
             print("Model test passed: ", ret["result"]["rdf"]["name"], ret["result"]["rdf"].get("id"))
         except:
             print("Model test failed: ", ret["result"]["rdf"]["name"], ret["result"]["rdf"].get("id"))
+
+    # One-off test of the cellpose model
+    image = imageio.v3.imread('https://static.imjoy.io/img/img02.png')
+    try:
+        ret = await triton.execute(
+            inputs=[image.astype('float32'), {'diameter': 30}],
+            model_name="cellpose-python",
+            decode_json=True
+        )
+        print(ret.keys())
+        print(ret)
+        mask = ret['mask'][0]
+        print("Model test passed: ", "Cellpose usinig cellpose-python")
+    except:
+
+        print("Model test failed: ", "Cellpose using cellpose-python")
+        traceback.print_exc()
+
+    # One-off test of the stardist model
+    image = imageio.v3.imread('https://static.imjoy.io/img/img02.png')
+    try:
+        ret = await triton.execute(
+            inputs=[image.astype('float32'), {'diameter': 30}],
+            model_name="stardist-python",
+            decode_json=True
+        )
+        print(ret.keys())
+        print(ret)
+        mask = ret['mask'][0]
+        print("Model test passed: ", "Stardist using stardist-python")
+    except:
+        print("Model test failed: ", "Cellpose using cellpose-python")
+        traceback.print_exc()
 
 asyncio.run(main())
